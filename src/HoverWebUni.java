739749e83862cc0922dad08bfec8497e500813a7
lhamutserings <lhamu.sherpa.us@gmail.com>
2022-11-03 11:46:37 -0400

    Hover feature added

---
+++ src/HoverWebUni.java
@@ -0,0 +1,42 @@
+//hover over a box and click the hidden text
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class HoverWebUni {
+    public static void main(String[] args) throws InterruptedException {
+       System.setProperty("webdriver.chrome.driver", "/Users/lhamusherpa/Documents/AUTOMATION/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.webdriveruniversity.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // got to actions
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[9]/a/div/div[1]/h1")).click();
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+
+      //driver.findElement(By.cssSelector("div.dropdown:nth-child(1) > button:nth-child(1)")).click();
+        ///driver.findElement(By.cssSelector("div.dropdown:nth-child(1) > div:nth-child(2) > a:nth-child(1)")).click();
+
+        Actions action = new Actions(driver);
+        WebElement hover = driver.findElement(By.className("dropbtn"));
+
+
+        int l = 2000;
+        action.moveToElement(hover).pause(l).moveToElement(driver.findElement(By.cssSelector("div.dropdown:nth-child(1) > div:nth-child(2) > a:nth-child(1)"))).click().build().perform();
+
+      //
+        //  WebElement hover = driver.findElement(By.xpath("html/body/div[13]/ul/li[4]/a"));
+        //action.moveToElement(we).moveToElement(webdriver.findElement(By.xpath("/expression-here"))).click().build().perform();
+        Thread.sleep(l);
+        driver.switchTo().alert().dismiss();
+
+        driver.close();
+        Thread.sleep(l);
+        driver.close();
+    }
+}
